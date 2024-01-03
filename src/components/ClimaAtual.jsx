9dd71beb87a962fc6bfe0586f7527b3ab5cf884b
Eder Henrique <ederhscc@gmail.com>
2024-01-03 15:32:40 -0300

    initial setup

---
+++ src/components/ClimaAtual.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClimaAtual = () => {
+  return (
+    <div>ClimaAtual</div>
+  )
+}
+
+export default ClimaAtual
---
+++ src/components/Previsao.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Previsao = () => {
+  return (
+    <div>Previsao</div>
+  )
+}
+
+export default Previsao
